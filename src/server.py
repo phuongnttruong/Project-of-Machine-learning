1e3fbcb56399591916f4c0bd042faae48ed4b831
Esko Arajärvi <esko.arajarvi@solita.fi>
2022-10-23 15:26:53 +0300

    Colored the map markers based on traffic data (mock version), and added simple form to the html page

--- src/server.py
+++ src/server.py
@@ -18,6 +18,8 @@ HELSINKI_TZ = "Europe/Helsinki"
 # Load all traffic data for 2021
 file_list = glob.glob(f"{DIGITRAFFIC_DATA_DIR}/traffic_averages_*.csv")
 traffic_data = pd.concat((pd.read_csv(f) for f in file_list), ignore_index=True)
+avg_speeds = traffic_data.groupby("location_id").median().get("avg_speed").to_dict()
+# print(avg_speeds)
 
 # Get list of unique station IDs
 station_ids = [str(int(x)) for x in traffic_data["location_id"].dropna().unique().tolist()]
@@ -29,22 +31,28 @@ with open(f"{DIGITRAFFIC_DATA_DIR}/station_data.json") as f:
 station_data = {x: all_stations[x] for x in station_ids if x in all_stations.keys()}
 #print(station_data)
 
-base_color = "#FF0000"
+base_color = 1.0
 
 urls = (
-    '/map', 'index'
+    '/', 'index'
 )
 
 web.config.debug = True
 
 
+def get_day_number(now):
+    return  now.timetuple().tm_yday
+
+
+def get_bin_number(now):
+    return now.hour * 12 + (now.minute // 5) * 5
+
+
 def get_latest_traffic():
     now = datetime.now(pytz.timezone("Europe/Helsinki"))
-    day_number = now.timetuple().tm_yday
-    bin_number = now.hour * 12 + (now.minute // 5) * 5
     return traffic_data.loc[
-        (traffic_data['day_number'] == day_number) &
-        (traffic_data['bin'] == bin_number) &
+        (traffic_data['day_number'] == get_day_number(now)) &
+        (traffic_data['bin'] == get_bin_number(now)) &
         (traffic_data['location_id'] != np.NaN)
     ]
 
@@ -60,32 +68,56 @@ def get_initial_params():
     return statistics.mean(latitudes), statistics.mean(longitudes), zoom
 
 
+def rgb_to_hex(rgb):
+    return "#%02x%02x%02x" % rgb
+
+
+def get_marker_color(point):
+    speed = point['avg_speed']
+    avg_speed_for_station = avg_speeds[point['location_id']]
+    print(speed, avg_speed_for_station)
+    if speed < 0.8 * avg_speed_for_station:
+        color = (255, 0, 0)
+    elif speed < 0.98 * avg_speed_for_station:
+        color = (255, 255, 0)
+    else:
+        color = (0, 255, 0)
+    return rgb_to_hex(color)
+
+
 def update_map(data_points):
     avg_lat, avg_long, zoom = get_initial_params()
     entry_map = folium.Map(
         location=[avg_lat, avg_long],
         zoom_start=zoom
     )
+
     for point in data_points.to_dict(orient="records"):
         this_station = station_data[str(int(point["location_id"]))]
+        marker_color = get_marker_color(point)
         print(this_station)
         folium.CircleMarker(
             location=(this_station["coordinates"][1], this_station["coordinates"][0]),
             radius=30,
-            color=base_color,
+            color=marker_color,
             fill=True,
-            # fill_color=base_color,
+            fill_color=marker_color,
         ).add_to(entry_map)
     return entry_map._repr_html_()
-    # entry_map.save(html_str)
+
+
+def get_html_template():
+    with open("templates/map.html") as f:
+        return f.read()
 
 
 class index:
     def GET(self):
         traffic_now = get_latest_traffic()
-        return update_map(traffic_now)
-        # with open("map.html", "r") as f:
-        #     return f.read()
+        html_template = get_html_template()
+        html = html_template.replace("%map_element%", update_map(traffic_now))
+        # html = html_template.replace("%delivery_estimate%", "")
+        return html
 
 
 if __name__ == "__main__":
